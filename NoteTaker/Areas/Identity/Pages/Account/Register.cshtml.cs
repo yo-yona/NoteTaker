2a116ad80cd2007a9210629fc04d0aa93b0ac915
yo-yona <svetlanya05@mail.ru>
2022-05-03 13:14:41 +0300

    Identity ok final

--- NoteTaker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ NoteTaker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -163,7 +163,7 @@ namespace NoteTaker.Areas.Identity.Pages.Account
             }
             catch
             {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
                     $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
